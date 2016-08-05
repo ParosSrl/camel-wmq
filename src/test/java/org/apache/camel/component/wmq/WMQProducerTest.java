ba664e44a55981863c7106b15dcac0a08b708818
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2016-08-05 10:04:51 +0200

    Fix log by adding slf4j-simple and update dependency version

--- src/test/java/org/apache/camel/component/wmq/WMQProducerTest.java
+++ src/test/java/org/apache/camel/component/wmq/WMQProducerTest.java
@@ -6,19 +6,19 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class WMQProducerTest extends CamelTestSupport {
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
         CamelContext context = super.createCamelContext();
         context.addRoutes(route());
-        System.out.println("creo!!!");
         return context;
     }
 
     @Test
     public void test() throws Exception {
-        System.out.println("mando!");
         template().sendBody("direct:input", "aa2e23rwe");
     }
 
@@ -28,8 +28,7 @@ public class WMQProducerTest extends CamelTestSupport {
             public void configure() throws Exception {
                 from("direct:input")
                     .log(body().toString())
-                    .setHeader("JMS_IBM_MQMD_ApplIdentityData", constant("aarfe2234"))
-                    .setHeader("JMS_IBM_MQMD_ApplIdentityData", constant("aarfe2234"))
+                    .setHeader("JMS_IBM_MQMD_ApplIdentityData", constant("APPLIDDATA_"+new Date()))
                     .to("wmq:QRM_AWS_ENTE_OUT?username=mqm&password=mqm");
             }
         };
